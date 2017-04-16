c70a416ace30830c6897da3178345f619a149c6c
shehaaz <shehaaz@gmail.com>
2017-04-16 14:08:52 -0700

    added fancyTimeToAngle

--- src/main/scala/com/epi/scala/clock/TimeToAngle.scala
+++ src/main/scala/com/epi/scala/clock/TimeToAngle.scala
@@ -2,6 +2,19 @@ package com.epi.scala.clock
 
 object TimeToAngle extends App {
 
+  def fancyTimeToAngle(h: Int, m: Int): Double = {
+    if(h<=0 || m<0 || h>12 || m>=60){
+      println(s"Invalid input: $h:$m")
+      0.0
+    } else {
+      val hour_angle = 0.5 * (h*60 + m)
+      val minute_angle = 6 * m
+      val angle = (hour_angle - minute_angle).abs
+      println(s"$h:$m => ${Math.min(360 - angle, angle)}")
+      Math.min(360 - angle, angle)
+    }
+  }
+
   def timeToAngle(hour: Int, minute: Int = 0): Double = {
     if(notValidInput(hour, minute)) {
       println(s"Invalid input: $hour:$minute")
@@ -46,8 +59,15 @@ object TimeToAngle extends App {
   override def main(args: Array[String]): Unit = {
     timeToAngle(12, 30)
     timeToAngle(3, 30)
+    timeToAngle(9, 0)
     timeToAngle(-1, 30)
     timeToAngle(1, 90)
+
+    fancyTimeToAngle(12, 30)
+    fancyTimeToAngle(3, 30)
+    fancyTimeToAngle(9, 0)
+    fancyTimeToAngle(-1, 30)
+    fancyTimeToAngle(1, 90)
   }
 
 }
